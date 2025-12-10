3a381745d9e098f14d663f51fac98b3f778786f1
phibuinek <phibuinek@gmail.com>
2025-12-10 18:15:01 +0700

    config cors

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,7 @@ async function bootstrap() {
       'http://localhost:3000',
       'http://localhost:3001',
       'https://nailweb.vercel.app',
+      'https://phamnailsupply.vercel.app',
       process.env.FRONTEND_URL,
     ].filter(Boolean),
     credentials: true,
